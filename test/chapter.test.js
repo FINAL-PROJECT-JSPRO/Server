dd20e789ab2e26e9d9c8ed041e887095befee32e
diazkautsar <diazkautsar77@gmail.com>
2020-03-23 00:22:10 +0700

    updated

--- test/chapter.test.js
+++ test/chapter.test.js
@@ -1,17 +1,18 @@
 const request = require('supertest')
 const app = require('../app')
-const { User, sequelize } = require('../models')
+const { User, Chapter, sequelize } = require('../models')
 const { queryInterface } = sequelize
 const { sign } = require('../helpers/jwt')
-const chapters = require('../dataForSeeder/chapters')
 
 let token
+let id_user
+let id_chapter
 
 describe('chapters router test', () => {
     beforeEach((done) => {
         User.create({
-            username: 'edytansil',
-            email: 'edytansil@mail.com',
+            username: 'edytansil2222',
+            email: 'edytansil2222@mail.com',
             password: "inipassword"
         })
             .then(user => {
@@ -19,14 +20,22 @@ describe('chapters router test', () => {
                     id: user.id,
                     email: user.email
                 })
+                id_user = user.id
                 done()
             })
             .catch(err => done(err))
     })
 
     beforeEach((done) => {
-        queryInterface.bulkInsert('Chapters', chapters, {})
-            .then(_ => done())
+        Chapter.create({
+            title: 'contoh title',
+            body: 'contoh body',
+            SubjectId: 1
+        })
+            .then(result => {
+                id_chapter = result.id
+                done()
+            })
             .catch(err => done(err))
     })
     afterEach((done) => {
@@ -36,13 +45,42 @@ describe('chapters router test', () => {
     })
 
     describe('test for get all chapter', () => {
-        test('it shoul be return array of obeject and status 200', (done) => {
+        test('it shoul be return array of object and status 200', (done) => {
             request(app)
                 .get('/chapters')
                 .set('access_token', token)
                 .end((err, response) => {
                     expect(err).toBe(null)
                     expect(response.body).toStrictEqual(expect.any(Array))
+                    done()
+                })
+        })
+    })
+
+    describe('test for get one chpater', () => {
+        beforeEach((done) => {
+            User.create({
+                username: 'budyhartanto',
+                email: 'iniemailbudi@mail.com',
+                password: "inipassword"
+            })
+                .then(user => {
+                    token = sign({
+                        id: user.id,
+                        email: user.email
+                    })
+                    id_user = user.id
+                    done()
+                })
+                .catch(err => done(err))
+        })
+        test('it should be return object and status 200', (done) => {
+            request(app)
+                .get(`/chapters/${id_chapter}`)
+                .set('access_token', token)
+                .end((err, response) => {
+                    expect(err).toBe(null)
+                    expect(response.body).toStrictEqual(expect.any(Object))
                     done()
                 })
         })
