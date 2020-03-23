6f449563ddd4557bcf03aeb8db8250d58ed204c4
ninoloid <muhammadsatriaadiputra@gmail.com>
2020-03-24 03:40:47 +0700

    tested, worked

--- routes/githubRouter.js
+++ routes/githubRouter.js
@@ -4,4 +4,5 @@ const { githubController } = require('../controllers')
 router.post('/token', githubController.getToken)
 router.post('/profile', githubController.getUser)
 router.post('/login', githubController.login)
+router.post('/addtorepo', githubController.addToRepo)
 module.exports = router
